a592d7240465e47381e634c2b903e1d553cb0202
mishellevf <mishelle.villarreal@utec.edu.pe>
2024-06-12 21:26:40 -0500

    a

---
+++ tests.py
@@ -0,0 +1,10 @@
+
+#####Pruebas de Desarrollo
+#Verificar que se está seteando bien las clases y que funcionan los métodos
+
+#Verificar que los métodos de el servicio API lance el statuscode sea 200
+#Verificar que el path del CSV se lea bien para usarlo
+#Verificar que existe la ciudad
+#Verificar que la distancia calculada sea correcta
+#Verificar que no se entregue la misma ciudad
+#Verificar que se pase bien el result label
